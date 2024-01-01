d3fd9acaba7825c03c7ffbcdf606dc6fff9c2502
Yuvraj Singh <yuvisingh478@gmail.com>
2024-01-01 17:20:00 +0530

    feature: added the dashboard with customers and invoices pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+// The `page` file holds special meaning
+// Each seprate folder inside root represents a url endpoint if it contains `page` file
+export default function Page() {
+    return <p>This is the dashboard</p>
+}
